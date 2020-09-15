6b7b980e082c707c30cc1ae3bb272193d3ccd1dd
Vishal Maurya <gaurav.singh141@gmail.com>
2020-09-16 00:11:46 +0530

    Project DocScanner: functions added

--- Projects/DocScanner.py
+++ Projects/DocScanner.py
@@ -1,6 +1,16 @@
 from cv2 import cv2
 import numpy as np
 
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThres = cv2.erode(imgDial,kernel,iterations=1)
+    return imgThres
+
 def DocScanner():
     widthImg = 540
     heightImg = 640
@@ -14,8 +24,8 @@ def DocScanner():
             continue
         cv2.resize(img, (widthImg, heightImg))
         imgCount = img.copy()
-        
 
-        cv2.imshow('Output', imgFinal)
+
+        cv2.imshow('Output', imgCount)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
