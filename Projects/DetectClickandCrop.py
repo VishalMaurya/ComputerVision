b9d2b120db6df017f89cb5ccb740acbc3b7d83cb
Vishal Maurya <gaurav.singh141@gmail.com>
2020-09-26 15:52:47 +0530

    Project Detect Click and Crop prospect: finished

--- Projects/DetectClickandCrop.py
+++ Projects/DetectClickandCrop.py
@@ -1,5 +1,8 @@
 from cv2 import cv2
 import numpy as np
+import random
+
+
 def wrap_perspective(img,edges):
     width, height = 250,350
     pts1 = np.float32([edges[0],edges[1],edges[2],edges[3]])
@@ -7,6 +10,15 @@ def wrap_perspective(img,edges):
     matrix = cv2.getPerspectiveTransform(pts1,pts2)
     imgOutput = cv2.warpPerspective(img,matrix,(width,height))
     cv2.imshow("Output Image ", imgOutput)
+
+    # if cv2.waitKey(1) & 0xFF == ord('s'):
+    #     cv2.imwrite("Projects/resources/scanned/Cropped_"+str(random.randint(1, 99))+".jpg",imgOutput)
+    #     cv2.rectangle(imgOutput,(0,200),(640,300),(0,255,0),cv2.FILLED)
+
+    #     cv2.putText(imgOutput,"Scan Saved",(150,265),cv2.FONT_HERSHEY_DUPLEX,
+    #                     2,(0,0,255),2)
+    #     cv2.imshow("Result",imgOutput)
+    #     cv2.waitKey(50)
 
 
 
@@ -21,7 +33,6 @@ def mousePoints(event,x,y,flags,params):
 def reorder (myPoints):
     # myPoints = myPoints.reshape((4,2))
     myPointsNew = np.zeros((4,1,2),np.int32)
-
     add = myPoints.sum(1)
     diff = np.diff(myPoints,axis=1)
     
@@ -44,13 +55,19 @@ def DetectClickandCrop(path):
             mypoint = reorder(edges)
             wrap_perspective(img, mypoint)
             counter = 0
+            img = cv2.imread(path)
+
 
         for x in range (0,4):
             cv2.circle(img,(edges[x][0],edges[x][1]),3,(0,255,0),cv2.FILLED)
 
+        
+
         cv2.imshow("Original Image ", img)
         cv2.setMouseCallback("Original Image ", mousePoints)
-        cv2.waitKey(1)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 path = './resources/cards.jpg'
 
